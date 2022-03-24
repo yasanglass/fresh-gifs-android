ec8143b98c0e323083f2a300672871001bda3b06
YASAN <yasandev@gmail.com>
2022-03-25 00:55:51 +0430

    Create AppModule.kt

---
+++ app/src/main/java/dev/yasan/fresh/gifs/di/AppModule.kt
@@ -0,0 +1,38 @@
+package dev.yasan.fresh.gifs.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply { setLevel(HttpLoggingInterceptor.Level.BODY) }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(provideHttpLoggingInterceptor()).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.giphy.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(provideOkHttpClient())
+            .build()
+    }
+
+}
