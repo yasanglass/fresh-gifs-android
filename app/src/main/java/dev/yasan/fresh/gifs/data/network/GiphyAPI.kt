39152320ded978803578207565098b1581fd7968
YASAN <yasandev@gmail.com>
2022-03-25 01:40:38 +0430

    Add giphy api key's default value

--- app/src/main/java/dev/yasan/fresh/gifs/data/network/GiphyAPI.kt
+++ app/src/main/java/dev/yasan/fresh/gifs/data/network/GiphyAPI.kt
@@ -6,19 +6,21 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
+const val GIPHY_API_KEY = "RhxNSsiCJCBOnIBGQAUU08w88Y9lVxik"
+
 interface GiphyAPI {
 
     @Headers("Accept: application/json")
     @GET("v1/gifs/trending")
     suspend fun fetchTrendingGifs(
-        @Query("api_key") apiKey: String,
+        @Query("api_key") apiKey: String = GIPHY_API_KEY,
     ): Response<GiphyResponse>
 
     @Headers("Accept: application/json")
     @GET("v1/gifs/trending")
     suspend fun fetchQueriedGifs(
-        @Query("api_key") apiKey: String,
         @Query("q") query: String,
+        @Query("api_key") apiKey: String = GIPHY_API_KEY,
     ): Response<GiphyResponse>
 
 }
